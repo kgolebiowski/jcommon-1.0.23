78f2be1dd9b7088517cae67eb5a540f087e1d01e
Krzysztof Gołębiowski <krzysiek.golebiowski@gmail.com>
2017-06-05 22:14:12 +0200

    Moved two SpreadsheetDate-owner variables out from DayDate and renamed according to their's meaning

--- src/main/java/org/jfree/date/SpreadsheetDate.java
+++ src/main/java/org/jfree/date/SpreadsheetDate.java
@@ -80,6 +80,12 @@ import java.util.Date;
  */
 public class SpreadsheetDate extends DayDate {
 
+    /** The serial number for 1 January 1900. */
+    private static final int EARLIEST_DATE_ORDINAL = 2;
+
+    /** The serial number for 31 December 9999. */
+    private static final int LATEST_DATE_ORDINAL = 2958465;
+
     /** For serialization. */
     private static final long serialVersionUID = -2039586705374454461L;
     
@@ -146,7 +152,7 @@ public class SpreadsheetDate extends DayDate {
      */
     public SpreadsheetDate(final int serial) {
 
-        if ((serial >= SERIAL_LOWER_BOUND) && (serial <= SERIAL_UPPER_BOUND)) {
+        if ((serial >= EARLIEST_DATE_ORDINAL) && (serial <= LATEST_DATE_ORDINAL)) {
             this.serial = serial;
         }
         else {
@@ -156,7 +162,7 @@ public class SpreadsheetDate extends DayDate {
 
         // the day-month-year needs to be synchronised with the serial number...
       // get the year from the serial date
-      final int days = this.serial - SERIAL_LOWER_BOUND;
+      final int days = this.serial - EARLIEST_DATE_ORDINAL;
       // overestimated because we ignored leap days
       final int overestimatedYYYY = 1900 + (days / 365);
       final int leaps = DayDate.leapYearCount(overestimatedYYYY);
